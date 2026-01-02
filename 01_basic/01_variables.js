b85137e8b566875b281a2ab5c1d0d4651df94b9d
sainishubham94 <36475100+sainishubham94@users.noreply.github.com>
2026-01-02 11:09:27 +0000

    node js

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+console.log('shubhamm saini')
+
+//VARIABLES
+
+const accoundID = 12343
+let email = 'saini@google.com'
+var name = 'shubham '
+let password = '12345'
+let phoneNumber;
+
+// prefer not to use va because if we want to change one variuable name which has name in other file also thatchanges the other variable also with the same name
+console.log(email);
+console.table([email, password, accoundID, phoneNumber] )
--- README.md
+++
@@ -1,2 +0,0 @@
-# 01_JS_Basic
-js basic for ecommerce
